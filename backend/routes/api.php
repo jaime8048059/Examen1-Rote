37efcfbe4562e48a1dcd85d7265ad8c06ba78a96
Jaime Clemente Ramirez <jaime.clemente@uab.bo.edu>
2025-08-03 21:46:14 -0400

    Authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\AuthController;
 
 Route::apiResource('marcas',MarcaController::class);
 Route::apiResource('producto',ProductoController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
